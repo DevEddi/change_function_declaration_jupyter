3980d5026cd3c9b18a4be89df9b803ed22b8e716
Edmilson Silva <56802572+DevEddi@users.noreply.github.com>
2022-05-01 13:52:56 -0300

    ajustando o notebook

--- test_tratamento_de_valores_ausentes.py
+++ test_tratamento_de_valores_ausentes.py
@@ -1,21 +1,23 @@
 import pytest
 from testbook import testbook
-import pandas as pd
-
-data_test= pd.read_csv('mamografia.csv')
-
 
 @testbook('./tratamento_de_valores_ausentes.ipynb', execute=True)
-def test_tratar_ausentes(tb):
+def test_retorna_substituicao_valores_ausentes(tb):
     tratar_ausentes = tb.ref("tratar_ausentes")
     lista = ['BI-RADS', 'Idade', 'Forma', ' Margem', ' Densidade', 'Severidade']
     assert tratar_ausentes(lista) == None
 
 @testbook('./tratamento_de_valores_ausentes.ipynb', execute=True)
-def test_verifica_moda_da_severidade_zero(tb):
+def test_retorna_substituicao_pela_moda(tb):
     moda_severidade = tb.ref("verifica_moda_da_severidade")
-    lista = data_test.columns
-    moda_benigno = data_test[(data_test['Severidade'] == 0)][lista].mode()[0]
-    data_test.loc[(data_test[lista].isnull()) & (data_test['Severidade'] == 0), lista] = moda_benigno
-    assert moda_severidade(lista) == None
+    lista = ['BI-RADS', 'Forma', ' Margem', ' Densidade', 'Severidade']
+    for lis in lista:
+        assert moda_severidade(lis) == None
+
+
+@testbook('./tratamento_de_valores_ausentes.ipynb', execute=True)
+def test_retorna_substituicao_pela_media(tb):
+    media_severidade = tb.ref("verifica_media_da_severidade")
+    assert media_severidade('Idade') == None
+
 
